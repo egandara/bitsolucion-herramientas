e0d9ba36ed33013f003e55d05a7fc40b5ec05fdb
Esteban Gandara Ampuero <esteban.gandara@externos.bci.cl>
2026-06-04 09:58:44 -0400

    gestor de jobs

---
+++ Views/Shared/_JobManagementTabs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NotebookValidator.Web.Views.Shared
+{
+    public class _JobManagementTabsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
